d5046facfdd34aa4b0e71360a84feaecdf6dc4bd
devahmadpk <ahmadirfan746@gmail.com>
2025-11-28 22:33:49 +0500

    Configured website routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,21 @@
 
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Landing from './modules/Landing.jsx'
+import Developer from './modules/Developer.jsx'
+import Writer from './modules/Writer.jsx'
 import './index.css'
 
 function App() {
 
   return (
     <>
-      <Landing />
+    <BrowserRouter>
+        <Routes>
+            <Route path="/" element={<Landing />}/>
+            <Route path="/developer" element={<Developer />}></Route>
+            <Route path="/writer" element={<Writer />}></Route>
+        </Routes>
+    </BrowserRouter>
     </>
   )
 }
