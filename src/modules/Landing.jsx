15b1053db94ebbf8600b78c284224aa7ad1ddeb7
devahmadpk <ahmadirfan746@gmail.com>
2025-11-28 22:23:32 +0500

    Created a landing page for portfolio selection

--- src/modules/Landing.jsx
+++ src/modules/Landing.jsx
@@ -5,13 +5,19 @@ function Landing() {
     return (
         <>
         {/* Main div that contains 50 - 50 split of both portfolios */}
-            <main className="flex w-full h-screen p-0">
-                <div className="w-1/2 bg-black">
-
+            <main className="flex flex-col w-full h-screen">
+                <div className="w-full bg-slate-950 font-mono h-1/2 text-white flex justify-center items-center
+                    hover:h-3/4 transition-all duration-500 ease-in-out
+                ">
+                        <h2 className="text-2xl">Web Developer</h2>
+                    
                 </div>
 
-                <div className="w-1/2 bg-red-500">
-
+                <div className="w-full bg-stone-100 text-black h-1/2 flex justify-center items-center
+                    hover:h-3/4 transition-all duration-500 ease-in-out
+                ">
+                        <h2 className="text-2xl">Blog Writer</h2>
+                    
                 </div>
             </main>
         </>
